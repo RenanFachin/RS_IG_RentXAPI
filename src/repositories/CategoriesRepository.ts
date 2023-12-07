35d601baf00dbc9d08096ec9291590fd6d0aa4c1
Renan <renan.fachin@hotmail.com>
2023-12-07 19:12:33 -0300

    feat: Cria repository da classe de category

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,35 @@
+import { Category } from '../model/Category'
+
+// Criando o DTO
+interface ICreateCategoryDTO {
+  name: string
+  description: string
+}
+
+class CategoriesRepository {
+  // Definindo que categoryas deve serguir as tipagens do model Category, que é um array
+  private categories: Category[]
+
+  constructor() {
+    // inicializando categories como vazio
+    this.categories = []
+  }
+
+  // Create é responsável por registrar algo dentro do array categories
+  // DTO -> Data Transfer Object: Responsável por passar os dados entre as camadas
+  create({ name, description }: ICreateCategoryDTO) {
+    // Instanciando a classe Category
+    const category = new Category()
+
+    // Adicionando os dados necessários
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    })
+
+    this.categories.push(category)
+  }
+}
+
+export { CategoriesRepository }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,20 @@
 import { Router } from 'express'
 
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
+// Criando a rota
 const categoriesRoutes = Router()
-
-// Definindo que categoryas deve serguir as tipagens do model Category, que é um array e que inicializa como vazio
-const categories: Category[] = []
+// Instânciando o repository da de categoria
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   // Dados vindos do body da requisição
   const { name, description } = request.body
 
-  // Instanciando a classe Category
-  const category = new Category()
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  })
+  // Utilizando o repository, para que ele faça a conexão com o db e registre
+  categoriesRepository.create({ name, description })
 
-  categories.push(category)
-
-  return response.status(201).json({ category })
+  return response.status(201).send()
 })
 
 export { categoriesRoutes }
