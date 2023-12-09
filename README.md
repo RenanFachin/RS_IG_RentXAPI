4068ac30b877e7a68292b739bcc3d303170697eb
Renan <renan.fachin@hotmail.com>
2023-12-09 00:11:50 -0300

    feat: Cria lógica para leitura e armazenamento dos dados do arquivo csv

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 API para registros de uma locadora de automóveis.
 
 
+### Regras de negócio
+- [x] Deve ser possível criar uma categoria
+- [x] Deve ser possível listar todas categorias existentes
+- [x] Deve ser possível realizar a leitura de um arquivo para criação de categorias
+- [x] Deve ser possível criar uma especificação
+
 <!-- Libs utilizadas -->
 <!-- https://www.npmjs.com/package/tsx -->
 <!-- https://www.npmjs.com/package/uuid -->
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,23 +1,56 @@
 import fs from 'node:fs'
 import { parse } from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 
+interface IImportCategory {
+  name: string
+  description: string
+}
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    // Fazendo a leitura do arquivo "por partes"
-    const stream = fs.createReadStream(file.path)
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    // Definindo o "delimitador" de leitura do arquivo
-    const parseFile = parse({
-      delimiter: ',',
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      // Fazendo a leitura do arquivo "por partes"
+      const stream = fs.createReadStream(file.path)
+
+      // array para armazenar as categorias
+      const categories: IImportCategory[] = []
+
+      // Definindo o "delimitador" de leitura do arquivo
+      const parseFile = parse({
+        delimiter: ',',
+      })
+
+      // pipe faz com que cada chunk lido, seja enviado para outro local
+      stream.pipe(parseFile)
+
+      // lendo linha a linha (assíncrono, promise)
+      parseFile
+        .on('data', async (line) => {
+          // console.log(line)
+          // retorno de cada line: ["name", "description"]
+          const [name, description] = line // desestruturando
+
+          categories.push({
+            name,
+            description,
+          })
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', (e) => {
+          reject(e)
+        })
     })
+  }
 
-    // pipe faz com que cada chunk lido, seja enviado para outro local
-    stream.pipe(parseFile)
-
-    // lendo linha a linha
-    parseFile.on('data', async (line) => {
-      console.log(line)
-    })
+  // Como o retorno do loadCategories é uma promise, precisamos definicar o método execute como async
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
