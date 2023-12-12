ab92dd4f82f060af8b0f6f995689b395c43aa1e6
Renan <renan.fachin@hotmail.com>
2023-12-12 15:09:22 -0300

    test: Cria testes unitários para o useCase de Criação de categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,54 @@
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+describe('Create Category', () => {
+  beforeEach(() => {
+    // Instanciar as variáveis
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    )
+  })
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'CategoryTeste',
+      description: 'CategoryDescriptionTest',
+    }
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    })
+
+    // Verificando se existe uma categoria com o nome passado no repository in memory
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    )
+
+    // console.log(categoryCreated)
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new category with an existing name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'CategoryTeste',
+        description: 'CategoryDescriptionTest',
+      }
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
