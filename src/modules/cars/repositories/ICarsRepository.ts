c54f8257b061007e32102af7921ccd8c8a807a56
Renan <renan.fachin@hotmail.com>
2023-12-13 23:39:28 -0300

    feat: Implementa busca por carros disponíveis

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,9 +1,16 @@
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO'
 import { Car } from '../infra/typeorm/entities/Car'
 
+export interface IFindAvailableDTO {
+  brand?: string
+  category_id?: string
+  name?: string
+}
+
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>
   findByLicensePlate(license_plate: string): Promise<Car>
+  findAvailable({ brand, category_id, name }: IFindAvailableDTO): Promise<Car[]>
 }
 
 export { ICarsRepository }
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -1,6 +1,6 @@
 import { ICreateCarDTO } from '../../dtos/ICreateCarDTO'
 import { Car } from '../../infra/typeorm/entities/Car'
-import { ICarsRepository } from '../ICarsRepository'
+import { ICarsRepository, IFindAvailableDTO } from '../ICarsRepository'
 
 class CarsRepositoryInMemory implements ICarsRepository {
   cars: Car[] = []
@@ -32,6 +32,28 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate)
+  }
+
+  async findAvailable({
+    brand,
+    category_id,
+    name,
+  }: IFindAvailableDTO): Promise<Car[]> {
+    // find -> retorna apenas o primeiro
+    // filter -> retorna uma lista que satisfaça a condição
+    const carsAvailable = this.cars.filter((car) => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car
+      }
+      return null
+    })
+
+    return carsAvailable
   }
 }
 
