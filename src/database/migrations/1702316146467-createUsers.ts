6f3d20a4b8c0c113b03c1f6454a21340ed7dc11b
Renan <renan.fachin@hotmail.com>
2023-12-15 17:39:06 -0300

    feat: Cria migration para tabela de alugueis (rentals)

--- src/database/migrations/1702316146467-createUsers.ts
+++ src/database/migrations/1702316146467-createUsers.ts
@@ -9,7 +9,8 @@ export class createUsers1702316146467 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
